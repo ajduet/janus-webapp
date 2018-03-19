f5e513eaa6ba7c42ded4be0ca986e146c2094624
Calvin <calvin.milliron@gmail.com>
2018-03-19 14:29:37 -0400

    Update simpleTrainee.ts

--- client-side/src/app/portals/Caliber/screening/entities/simpleTrainee.ts
+++ client-side/src/app/portals/Caliber/screening/entities/simpleTrainee.ts
@@ -2,5 +2,8 @@
 //DB Entity for the potential employee being screened
 export interface SimpleTrainee{
     traineeID: number;
+    firstname: string;
+    lastname: string;
+    trackId: number;
     schedule: Date;//for the scheduled start time
 }
