15be941daa5a95cedf37ab958f7c611f08818907
CarpizoeE <you@example.com>
2018-03-20 09:58:09 -0400

    User Story 8

---
+++ client-side/src/app/portals/Caliber/screening/components/candidates-screening-list/candidates-screening-list.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Candidate } from '../../models/candidate';
+import { SearchPipe } from './search.pipe';
+import { NgxPaginationModule } from 'ngx-pagination'; // <-- import the module
+import { NAMES } from './dummy-data';
+@Component({
+  selector: 'app-candidates-screening-list',
+  templateUrl: './candidates-screening-list.component.html',
+  styleUrls: ['./candidates-screening-list.component.css']
+})
+export class CandidatesScreeningListComponent implements OnInit {
+
+  //  Dummy data for testing search bar
+  candidates: any[] = NAMES;
+  candidatesLength: number;
+  date = new Date();
+  today = this.date.getTime();
+  tomorrow = this.date.setDate(this.date.getDate() + 1);
+  dayAfterTomorrow = this.date.setDate(this.date.getDate() + 3);
+  tracks = [
+    'Java',
+    'C++',
+    'Selenium',
+    'Business Analyst',
+    '.NET',
+    'Other',
+  ];
+
+  beginScreening = false;
+  constructor(private http: HttpClientModule) {
+    this.candidatesLength = this.candidates.length;
+  }
+
+  ngOnInit() {
+  }
+  // [style.background-color]="beginScreeningPrompt()"
+  beginScreeningPrompt() {
+    if (this.beginScreening) {
+      return "block";
+    } else {
+      return "none";
+    }
+  }
+
+  getTodaysDate() {
+      this.today = new Date().getTime();
+      console.log(this.today);
+  }
+
+  getTomorrow(){
+    let date = new Date();
+    this.tomorrow = this.date.setDate(this.date.getDate() + 1);
+      console.log(this.tomorrow);
+  }
+
+  getNext3Days(){
+    let date = new Date();
+    this.dayAfterTomorrow = this.date.setDate(this.date.getDate() + 2);
+    console.log(this.dayAfterTomorrow);
+  }
+}
