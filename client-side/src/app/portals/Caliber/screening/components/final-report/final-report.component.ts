09b0638d442d08fa0cf57f8678822e4d9292fa56
Imperfect1987 <JamesWRules@gmail.com>
2018-03-22 13:20:42 -0400

    Styling and mock data for the final page

--- client-side/src/app/portals/Caliber/screening/components/final-report/final-report.component.ts
+++ client-side/src/app/portals/Caliber/screening/components/final-report/final-report.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./final-report.component.css']
 })
 export class FinalReportComponent implements OnInit {
+softSkillString: String;
+bucketStringArray: String[];
+overallScoreString: String;
+generalNotesString: String;
+
+
 
   constructor() { }
 
   ngOnInit() {
+    this.softSkillString = "Soft Skills: Pass";
+    this.bucketStringArray = ["20/30 OOP", "24/30 Basics", "12/20 Advanced", "09/10 SQL", "06/10 Web"];
+    this.overallScoreString = "Overall: 71%";
+    this.generalNotesString = "Answers were clear and concise. Wonderful attitude.";
+   
   }
 
 }
