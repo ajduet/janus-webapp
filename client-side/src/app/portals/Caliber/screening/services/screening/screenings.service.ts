f56d70084851e2a5f6847202083eb27477ba5cf2
Kirik0 <mkkim6@asu.edu>
2018-03-22 12:56:14 -0400

    Averted Gitastrophe, styled intro component

--- client-side/src/app/portals/Caliber/screening/services/screening/screenings.service.ts
+++ client-side/src/app/portals/Caliber/screening/services/screening/screenings.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-// import { Screening } from '../../entities/screening';
+import { Screening } from '../../entities/screening';
 
 @Injectable()
 export class ScreeningService {
