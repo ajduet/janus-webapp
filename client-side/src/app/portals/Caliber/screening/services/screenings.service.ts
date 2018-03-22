51bc260e918e4fc8b178b7c2adf82a94ad3b8b7d
Kirik0 <mkkim6@asu.edu>
2018-03-22 11:47:13 -0400

    Added SoftSkillsViolationService

--- client-side/src/app/portals/Caliber/screening/services/screenings.service.ts
+++ client-side/src/app/portals/Caliber/screening/services/screenings.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { Screening } from '../entities/screening';
@@ -6,6 +7,12 @@ import { Screening } from '../entities/screening';
 @Injectable()
 export class ScreeningsService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
+  public screeningID$ : Observable<Screening>;
+
+  retrieveScreening(): Observable<Screening>{
+    this.screeningID$ = this.http.post<Screening>('', {})
+    return this.screeningID$;
+  }
 }
