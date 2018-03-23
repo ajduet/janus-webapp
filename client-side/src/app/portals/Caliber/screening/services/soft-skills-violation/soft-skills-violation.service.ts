16592b3d5d4d9782376bd2a62f1b4d63412a5875
Leonard Blumberg <tlblumberg@gmail.com>
2018-03-23 09:56:20 -0400

    Friday morning commit

--- client-side/src/app/portals/Caliber/screening/services/soft-skills-violation/soft-skills-violation.service.ts
+++ client-side/src/app/portals/Caliber/screening/services/soft-skills-violation/soft-skills-violation.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
 import { SoftSkillViolation } from '../../entities/softSkillViolation'
@@ -13,10 +14,14 @@ export class SoftSkillsViolationService {
   public violationComments : string;
   public allViolations : SoftSkillViolation[];
   public selectedViolations : SoftSkillViolation[];
-
+  readonly violationURL: string = "violation/delete";
   public userSoftSkillViolations : SoftSkillViolation[];
 
-  addViolations(newViolations : SoftSkillViolation[], comments : string){
+  getPreviousViolations(): Observable<SoftSkillViolation[]>{
+    return this.http.get<SoftSkillViolation[]>(this.violationURL);
+  }
+
+  addViolations(newViolations : SoftSkillViolation[], comments : string[]){
     //Loop through newViolations and add any that are not already in selectedViolations.
     let exists : boolean = false;
     let currentSoftSkillViolation : SoftSkillViolation;
@@ -41,7 +46,7 @@ export class SoftSkillsViolationService {
 
       //Populate the currentSoftSkillViolation object
       currentSoftSkillViolation.violationType = newViolations[i].violationType;
-      currentSoftSkillViolation.Comment = comments;
+      currentSoftSkillViolation.Comment = comments[i];
       currentSoftSkillViolation.Time = new Date();
       currentSoftSkillViolation.violationID = null;
       //Update when we decide where to save screeningID
@@ -69,9 +74,13 @@ export class SoftSkillsViolationService {
     this.timestampList.push(new Date());
   }
 
-  deleteViolation(violationID: number){
-      
+  deleteViolation(violationID: number): Observable<SoftSkillViolation[]>{
+    let params = new HttpParams().set('id', violationID.toString());
+    return this.http.delete<SoftSkillViolation[]>(this.violationURL);
   }
 
+  handleDelete404() {
+
+  }
 
 }
