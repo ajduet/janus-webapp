59539b21aed2852b2178df731753f6494fdf817b
CarpizoeE <you@example.com>
2018-03-23 16:47:08 -0400

    So many services.... so little time (jk)

--- client-side/src/app/portals/Caliber/screening/components/candidates-screening-list/candidates-screening-list.component.ts
+++ client-side/src/app/portals/Caliber/screening/components/candidates-screening-list/candidates-screening-list.component.ts
@@ -1,29 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 // Pipes
-import { SearchPipe } from '../../util/search.pipe';
+import { SearchPipe } from "../../util/search.pipe";
 
 // Classes
-import { SimpleTrainee } from '../../entities/simpleTrainee';
-import { SkillType } from '../../entities/skillType';
+import { SimpleTrainee } from "../../entities/simpleTrainee";
+import { SkillType } from "../../entities/skillType";
 
 // Services
-import { SimpleTraineeService } from '../../services/simpleTrainee/simple-trainee.service';
-import { SkillTypeService } from '../../services/skillType/skill-type.service';
+import { SimpleTraineeService } from "../../services/simpleTrainee/simple-trainee.service";
+import { SkillTypeService } from "../../services/skillType/skill-type.service";
 
 // Installed Modules
 // npm install ngx-pagination --save
-import { NgxPaginationModule } from 'ngx-pagination'; // <-- import the module
+import { NgxPaginationModule } from "ngx-pagination"; // <-- import the module
+import { ScreeningService } from "../../services/screening/screenings.service";
 
 @Component({
-  selector: 'app-candidates-screening-list',
-  templateUrl: './candidates-screening-list.component.html',
-  styleUrls: ['./candidates-screening-list.component.css']
+  selector: "app-candidates-screening-list",
+  templateUrl: "./candidates-screening-list.component.html",
+  styleUrls: ["./candidates-screening-list.component.css"]
 })
 export class CandidatesScreeningListComponent implements OnInit {
-
   /* ###########################
         FIELDS
   ########################### */
@@ -32,13 +32,17 @@ export class CandidatesScreeningListComponent implements OnInit {
   skillTypes: SkillType[];
 
   selectedCandidate: SimpleTrainee;
-  beginScreening = false;
+  showBeginScreeningPrompt = false;
+  
   /* ###########################
        CONSTRUCTOR and INIT
   ########################### */
-  constructor(private http: HttpClientModule, private simpleTraineeService: SimpleTraineeService,
-    private skillTypeService: SkillTypeService) {
-  }
+  constructor(
+    private http: HttpClientModule,
+    private simpleTraineeService: SimpleTraineeService,
+    private skillTypeService: SkillTypeService,
+    private screeningService: ScreeningService
+  ) {}
 
   ngOnInit() {
     this.simpleTraineeService.getSimpleTrainees().subscribe(data => {
@@ -53,9 +57,8 @@ export class CandidatesScreeningListComponent implements OnInit {
         FUNCTIONS
   ########################### */
 
-  // [style.background-color]="beginScreeningPrompt()"
-  beginScreeningPrompt() {
-    if (this.beginScreening) {
+  toggleBeginScreeningPrompt() {
+    if (this.showBeginScreeningPrompt) {
       return "block";
     } else {
       return "none";
@@ -64,5 +67,17 @@ export class CandidatesScreeningListComponent implements OnInit {
 
   confirmSelectedCandidate(): void {
     this.simpleTraineeService.setSelectedCandidate(this.selectedCandidate);
+  }
+  
+  beginScreening(): void {
+    // create a new screening entry in the database
+   this.screeningService.beginScreening(
+          this.selectedCandidate.traineeID,
+          new Date(), 1,
+          this.selectedCandidate.skillTypeID
+        );
+    // retrieve the screening ID from the screening service
+    // and save the screening ID as a cookie to localStorage.
+    localStorage.setItem("screeningID", this.screeningService.getScreeningID.toString());
   }
 }
