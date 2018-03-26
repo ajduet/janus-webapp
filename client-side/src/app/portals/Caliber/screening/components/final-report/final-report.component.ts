cb0f3dca26d0a5842982c758019f744c74b68769
Kirik0 <mkkim6@asu.edu>
2018-03-26 14:01:22 -0400

    Added candidate names to the top of each view

--- client-side/src/app/portals/Caliber/screening/components/final-report/final-report.component.ts
+++ client-side/src/app/portals/Caliber/screening/components/final-report/final-report.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ScreeningService } from '../../services/screening/screenings.service';
+import { SimpleTraineeService } from '../../services/simpleTrainee/simple-trainee.service';
 
 @Component({
   selector: 'app-final-report',
@@ -11,12 +12,15 @@ softSkillString: String;
 bucketStringArray: String[];
 overallScoreString: String;
 generalNotesString: String;
+public candidateName: string;
 
 
-
-  constructor(private screeningService: ScreeningService) { }
+  constructor(private screeningService: ScreeningService,
+    private simpleTraineeService: SimpleTraineeService) { }
 
   ngOnInit() {
+    this.candidateName = this.simpleTraineeService.getSelectedCandidate().firstname + " " +
+                          this.simpleTraineeService.getSelectedCandidate().lastname;
     this.softSkillString = "Soft Skills: " + this.screeningService.softSkillsResult;
     this.bucketStringArray = ["20/30 OOP", "24/30 Basics", "12/20 Advanced", "09/10 SQL", "06/10 Web"];
     this.overallScoreString = "Overall: 71%";
