342990c398a48ca49dadef36e34abcd2514e2074
Imperfect1987 <JamesWRules@gmail.com>
2018-03-26 13:38:54 -0400

    Does copy to clipboard without plugins

--- client-side/src/app/portals/Caliber/screening/components/final-report/final-report.component.ts
+++ client-side/src/app/portals/Caliber/screening/components/final-report/final-report.component.ts
@@ -1,27 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import { ScreeningService } from '../../services/screening/screenings.service';
 
 @Component({
   selector: 'app-final-report',
   templateUrl: './final-report.component.html',
   styleUrls: ['./final-report.component.css']
 })
+
 export class FinalReportComponent implements OnInit {
-softSkillString: String;
-bucketStringArray: String[];
-overallScoreString: String;
-generalNotesString: String;
+//Strings of each part
+softSkillString: string;
+bucketStringArray: string[];
+overallScoreString: string;
+generalNotesString: string;
+//Compounded Strings
+allTextString: string;
 
 
 
-  constructor(private screeningService: ScreeningService) { }
+
+  constructor() { }
 
   ngOnInit() {
-    this.softSkillString = "Soft Skills: " + this.screeningService.softSkillsResult;
-    this.bucketStringArray = ["20/30 OOP", "24/30 Basics", "12/20 Advanced", "09/10 SQL", "06/10 Web"];
+    //Hardcoded Data
+    this.softSkillString = "Soft Skills: Pass";
+    this.bucketStringArray = ["\n" + "20/30 OOP","\n" +  "24/30 Basics","\n" +  "12/20 Advanced","\n" +  "09/10 SQL","\n" + "06/10 Web"];
     this.overallScoreString = "Overall: 71%";
-    this.generalNotesString = this.screeningService.generalComments;
-   
+    this.generalNotesString = "Answers were clear and concise. Wonderful attitude.";
+    this.allTextString = this.softSkillString + "\n" + this.bucketStringArray + "\n" + this.overallScoreString + "\n" + this.generalNotesString;
+    
+  }
+
+  //Used for copying the data to the clipboard (this is done using ngx-clipboard)
+  copyToClipboard(){
+
+    let selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = this.allTextString;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+
   }
 
 }
