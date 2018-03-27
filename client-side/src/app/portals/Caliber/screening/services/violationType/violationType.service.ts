c7c9235e04f18283ab9b97eff7b8c3a65c5e9be7
Leonard Blumberg <tlblumberg@gmail.com>
2018-03-27 10:50:12 -0400

    Trying to undo changes

--- client-side/src/app/portals/Caliber/screening/services/violationType/violationType.service.ts
+++ client-side/src/app/portals/Caliber/screening/services/violationType/violationType.service.ts
@@ -4,8 +4,6 @@ import { ViolationType } from '../../entities/violationType';
 import { VIOLATION_TYPES } from '../../mock-data/mock-violationTypes';
 import { of } from 'rxjs/observable/of';
 
-
-// Used to obtain an observable of mock Violation types
 @Injectable()
 export class ViolationTypeService {
 
